c7c6f0d3af58b4439dff371bc8435420a26eb16d
Aayush Kapoor <xeoneux@icloud.com>
2016-03-31 02:19:45 +0530

    Add sidebar animation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,4 +18,15 @@
         var me = $(this);
         alert(me.text());
     });
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
 })();
