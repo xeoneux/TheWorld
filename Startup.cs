2aa3e41db0c19f03717574663e5c0671e1c85868
Aayush Kapoor <xeoneux@icloud.com>
2016-03-30 23:46:25 +0530

    Remove IIS

--- Startup.cs
+++ Startup.cs
@@ -20,8 +20,6 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
