8832cab6111522377bb0a1fca1f958395af9e7af
Aayush Kapoor <xeoneux@icloud.com>
2016-03-31 01:08:46 +0530

    Add javascript event

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 
 var ele = document.getElementById("username");
 ele.innerHTML = "Aayush Kapoor";
+
+var main = document.getElementById("main");
+main.onmouseenter = function() {
+    main.style["background-color"] = "#888";
+};
+main.onmouseleave = function() {
+    main.style["background-color"] = "";
+};
