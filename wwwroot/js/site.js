7e16638351b8c1a3c9d3fefc49fc3167df7ec10b
Aayush Kapoor <xeoneux@icloud.com>
2016-03-31 01:13:16 +0530

    Wrap code in IIFE

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,12 +1,14 @@
 // site.js
 
-var ele = document.getElementById("username");
-ele.innerHTML = "Aayush Kapoor";
+(function() {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Aayush Kapoor";
 
-var main = document.getElementById("main");
-main.onmouseenter = function() {
-    main.style["background-color"] = "#888";
-};
-main.onmouseleave = function() {
-    main.style["background-color"] = "";
-};
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style["background-color"] = "#888";
+    };
+    main.onmouseleave = function() {
+        main.style["background-color"] = "";
+    };
+})();
