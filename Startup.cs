a747d801b0ed8e6753723918299bb47708213257
Aayush Kapoor <xeoneux@icloud.com>
2016-03-30 23:57:39 +0530

    Serve static files

--- Startup.cs
+++ Startup.cs
@@ -20,10 +20,8 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync($"Hello World!: {context.Request.Path}");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
 
         // Entry point for the application.
