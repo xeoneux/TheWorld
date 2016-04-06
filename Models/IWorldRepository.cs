3b82c34eec3ba506493ef70551f82ed97c2904cf
Aayush Kapoor <xeoneux@icloud.com>
2016-04-06 19:10:03 +0530

    Create world repository

---
+++ Models/IWorldRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace TheWorld.Models
+{
+    public interface IWorldRepository
+    {
+        IEnumerable<Trip> GetAllTrips();
+
+        IEnumerable<Trip> GetAllTripsWithStops();
+    }
+}
---
+++ Models/WorldRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Entity;
+
+namespace TheWorld.Models
+{
+    public class WorldRepository : IWorldRepository
+    {
+        private WorldContext _context;
+
+        public WorldRepository(WorldContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            return _context
+                .Trips
+                .OrderBy(t => t.Name)
+                .ToList();
+        }
+
+        public IEnumerable<Trip> GetAllTripsWithStops()
+        {
+            return _context.Trips
+                .Include(t => t.Stops)
+                .OrderBy(t => t.Name)
+                .ToList();
+        }
+    }
+}
