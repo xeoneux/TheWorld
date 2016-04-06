7289cb01d625f079281bab5a4f91ea47d9050281
Aayush Kapoor <xeoneux@icloud.com>
2016-04-06 15:16:23 +0530

    Add developer exception page

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,7 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseMvc(config =>
             {
