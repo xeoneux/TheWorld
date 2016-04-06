ccc3aac1b113c88d5f67d708a863aa6f735e6f37
Aayush Kapoor <xeoneux@icloud.com>
2016-04-06 22:29:11 +0530

    Add GET method to API

---
+++ Controllers/Api/TripController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNet.Mvc;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        private IWorldRepository _repository;
+
+        public TripController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+            var results = _repository.GetAllTripsWithStops();
+
+            return Json(results);
+        }
+    }
+}
