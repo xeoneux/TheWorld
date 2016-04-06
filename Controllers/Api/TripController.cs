1261e580a0813f4fa260663a1009092a2d5858f8
Aayush Kapoor <xeoneux@icloud.com>
2016-04-06 23:38:43 +0530

    Validate trip on POST

--- Controllers/Api/TripController.cs
+++ Controllers/Api/TripController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using Microsoft.AspNet.Mvc;
 using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Api
 {
@@ -23,9 +25,16 @@ namespace TheWorld.Controllers.Api
         }
 
         [HttpPost("")]
-        public JsonResult Post([FromBody]Trip newTrip)
+        public JsonResult Post([FromBody]TripViewModel newTrip)
         {
-            return Json(true);
+            if (ModelState.IsValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Created;
+                return Json(true);
+            }
+
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return Json(new { Message = "Failed", ModelState = ModelState });
         }
     }
 }
