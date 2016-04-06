acf94058b5574dfd55346608649c4c1ed56d6d1b
Aayush Kapoor <xeoneux@icloud.com>
2016-04-06 16:37:31 +0530

    Fix typo

--- Models/Stop.cs
+++ Models/Stop.cs
@@ -7,7 +7,7 @@ namespace TheWorld.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public double Latitude { get; set; }
-        public double Longitute { get; set; }
+        public double Longitude { get; set; }
         public DateTime Arrival { get; set; }
         public int Order { get; set; }
     }
