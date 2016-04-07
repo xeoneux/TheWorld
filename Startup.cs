8041d0dd56e2075a10b5dd99e9ab0929bbddf117
Aayush Kapoor <xeoneux@icloud.com>
2016-04-07 17:42:05 +0530

    Configure identity

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -37,6 +38,14 @@ namespace TheWorld
 
             services.AddLogging();
 
+            services.AddIdentity<WorldUser, IdentityRole>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+                config.Password.RequiredLength = 8;
+                config.Cookies.ApplicationCookie.LoginPath = "/Auth/Login";
+            })
+                .AddEntityFrameworkStores<WorldContext>();
+
             services.AddEntityFramework()
                 .AddSqlServer()
                 .AddDbContext<WorldContext>();
@@ -58,6 +67,8 @@ namespace TheWorld
             app.UseDeveloperExceptionPage();
 
             app.UseStaticFiles();
+            
+            app.UseIdentity();
 
             Mapper.Initialize(config =>
             {
