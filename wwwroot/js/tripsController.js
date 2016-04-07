bdfcdb43d1f942527354a77c215c1dbc712d6ef6
Aayush Kapoor <xeoneux@icloud.com>
2016-04-08 01:10:31 +0530

    Fetch trips data from API

--- wwwroot/js/tripsController.js
+++ wwwroot/js/tripsController.js
@@ -5,21 +5,21 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
 
-        vm.trips = [
-            {
-                name: "India Trip",
-                created: new Date()
-            },
-            {
-                name: "World Trip",
-                created: new Date()
-            }
-        ];
+        vm.trips = [];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+
+        $http.get("/api/trips")
+            .then(function(response) {
+                angular.copy(response.data, vm.trips);
+            }, function(error) {
+                vm.errorMessage = "Failed to load data: " + error;
+            });
 
         vm.AddTrip = function() {
             vm.trips.push({
