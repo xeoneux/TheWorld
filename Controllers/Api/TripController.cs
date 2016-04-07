d429ff73fa9e21f3c32151fa5059a6e5ba4f2c8e
Aayush Kapoor <xeoneux@icloud.com>
2016-04-07 11:50:37 +0530

    Log trip activity

--- Controllers/Api/TripController.cs
+++ Controllers/Api/TripController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using AutoMapper;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
 using TheWorld.Models;
 using TheWorld.ViewModels;
 
@@ -12,10 +13,12 @@ namespace TheWorld.Controllers.Api
     public class TripController : Controller
     {
         private IWorldRepository _repository;
+        private ILogger<TripController> _logger;
 
-        public TripController(IWorldRepository repository)
+        public TripController(IWorldRepository repository, ILogger<TripController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         [HttpGet("")]
@@ -35,12 +38,15 @@ namespace TheWorld.Controllers.Api
                 {
                     var newTrip = Mapper.Map<Trip>(vm);
 
+                    _logger.LogInformation("Attempting to save a new trip");
+
                     Response.StatusCode = (int)HttpStatusCode.Created;
                     return Json(Mapper.Map<TripViewModel>(newTrip));
                 }
             }
             catch (Exception ex)
             {
+                _logger.LogError("Failed to save new trip", ex);
                 Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 return Json(new { Message = ex.Message });
             }
