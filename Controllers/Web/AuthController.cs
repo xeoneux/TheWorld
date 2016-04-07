43e8554f739ff369c02940b8ed89990f885f500d
Aayush Kapoor <xeoneux@icloud.com>
2016-04-07 18:09:42 +0530

    Login using SignInManager

--- Controllers/Web/AuthController.cs
+++ Controllers/Web/AuthController.cs
@@ -1,14 +1,52 @@
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
+using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AuthController : Controller
     {
+        private SignInManager<WorldUser> _signInManager;
+
+        public AuthController(SignInManager<WorldUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
             {
                 RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, true, false);
+
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Trips", "App");
+                    }
+                    else
+                    {
+                        return Redirect(returnUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password is incorrect");
+                }
             }
 
             return View();
