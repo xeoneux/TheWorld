efd76207337be70df6c8ea63ad3df112ba578de1
Aayush Kapoor <xeoneux@icloud.com>
2016-04-07 12:32:33 +0530

    Add validations to stop view model

--- ViewModels/StopViewModel.cs
+++ ViewModels/StopViewModel.cs
@@ -1,13 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TheWorld.ViewModels
 {
     public class StopViewModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
         public string Name { get; set; }
+
         public double Latitude { get; set; }
+
         public double Longitude { get; set; }
+
+        [Required]
         public DateTime Arrival { get; set; }
     }
 }
