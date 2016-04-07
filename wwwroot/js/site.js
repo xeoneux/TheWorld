d98147b762afaed669514e6506f2f33fef4bdff1
Aayush Kapoor <xeoneux@icloud.com>
2016-04-07 18:29:37 +0530

    Remove jQuery functions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,18 +1,6 @@
 // site.js
 
 (function() {
-
-    var ele = $("#username");
-    ele.text("Aayush Kapoor");
-
-    var main = $("#main");
-    main.on("mouseenter", function() {
-        main.css("background-color", "#888");
-    });
-    main.on("mouseleave", function() {
-        main.css("background-color", "");
-    });
-
     var $sidebarAndWrapper = $("#sidebar, #wrapper");
     var $icon = $("#sidebarToggle i.fa");
 
