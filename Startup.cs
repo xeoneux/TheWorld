d0683beaea6f67460cbcddd56c6a7f7bf70546e4
Aayush Kapoor <xeoneux@icloud.com>
2016-04-07 12:15:25 +0530

    Add stop view model

--- Startup.cs
+++ Startup.cs
@@ -60,6 +60,7 @@ namespace TheWorld
             Mapper.Initialize(config =>
             {
                 config.CreateMap<Trip, TripViewModel>().ReverseMap();
+                config.CreateMap<Stop, StopViewModel>().ReverseMap();
             });
 
             app.UseMvc(config =>
---
+++ ViewModels/StopViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TheWorld.ViewModels
+{
+    public class StopViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Arrival { get; set; }
+    }
+}
