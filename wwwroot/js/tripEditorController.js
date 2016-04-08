bcccd0363e764e3c9e9a10bd7e756c96989dc36d
Aayush Kapoor <xeoneux@icloud.com>
2016-04-08 15:53:22 +0530

    GET stops data from server API

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -5,12 +5,22 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         vm.tripName = $routeParams.tripName;
         vm.stops = [];
         vm.isBusy = true;
         vm.errorMessage = "";
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function(response) {
+                angular.copy(response.data, vm.stops);
+            }, function(error) {
+                vm.errorMessage = "Failed to load stops";
+            })
+            .finally(function() {
+                vm.isBusy = false;
+            });
     }
 })();
