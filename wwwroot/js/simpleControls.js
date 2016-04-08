6f59762038036aebfbd28798c86b3abc950bb6d0
Aayush Kapoor <xeoneux@icloud.com>
2016-04-08 13:17:42 +0530

    Create simple controls module with wait cursor

---
+++ wwwroot/js/simpleControls.js
@@ -0,0 +1,13 @@
+// simpleControls.js
+
+(function() {
+    "use strict";
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+})();
