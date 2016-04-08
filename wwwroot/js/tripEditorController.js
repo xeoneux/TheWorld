7c46f1d015456c98290332f76ed0efd3cd80ff46
Aayush Kapoor <xeoneux@icloud.com>
2016-04-08 16:17:54 +0530

    Show map with stops

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -16,11 +16,31 @@
         $http.get("/api/trips/" + vm.tripName + "/stops")
             .then(function(response) {
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
             }, function(error) {
                 vm.errorMessage = "Failed to load stops";
             })
             .finally(function() {
                 vm.isBusy = false;
             });
+    }
+
+    function _showMap(stops) {
+        if (!!stops && stops.length > 0) {
+            var mapStops = _.map(stops, function(stop) {
+                return {
+                    lat: stop.latitude,
+                    long: stop.longitude,
+                    info: stop.name
+                };
+            });
+
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
     }
 })();
