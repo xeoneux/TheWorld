5dd0af123701455ef4ce5e2e84e80dd1a922a4ea
Aayush Kapoor <xeoneux@icloud.com>
2016-04-08 15:15:29 +0530

    Add angular router

--- wwwroot/js/app-trips.js
+++ wwwroot/js/app-trips.js
@@ -2,5 +2,18 @@
 
 (function() {
     "use strict";
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function($routeProvider) {
+            $routeProvider
+                .when("/", {
+                    controller: "tripsController",
+                    controllerAs: "vm",
+                    templateUrl: "/views/tripsView.html"
+                });
+
+            $routeProvider
+                .otherwise({
+                    redirectTo: "/"
+                });
+        });
 })();
