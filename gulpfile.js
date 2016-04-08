70699715342d6a24a80a4032c1cddafa031f3fb5
Aayush Kapoor <xeoneux@icloud.com>
2016-04-08 17:07:20 +0530

    Minify JS using uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require("gulp");
+var uglify = require("gulp-uglify");
 
-gulp.task("default", function() {
-    
+gulp.task("minify", function() {
+    return gulp.src("wwwroot/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("wwwroot/lib/"))
 });
